ccaaa1be5aa90e24f3958bcb4b6de9b73b4b0ec9
Matt Woods <mattwoods21@gmail.com>
2017-12-21 00:38:05 -0600

    working prompt

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const fetch = require('node-fetch');
+const prompt = require('prompt');
 
-fetch(`https://swapi.co/api/planets`)
-  .then((res) => {
-    return res.json();
-  })
-  .then((json) => {
-    console.log(json);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+prompt.start();
+
+prompt.get(['query'], function (err, result) {
+  fetch(`https://swapi.co/api/${result.query}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
